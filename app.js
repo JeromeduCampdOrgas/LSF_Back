30da5805a1d1b58c1ec41c391cdac739ef1ab3eb
Jerome <ducampdorgasjerome@gmail.com>
2022-04-16 13:50:56 +0200

    Access-Control-Allow-Origin correction

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://lsf-front.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://lsf-front.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
